641b2c36d077f236933f40d99829f419023be5b8
= <fellan12@gmail.com>
2017-09-05 09:42:43 +0200

    Done with Codes

--- Felix/Codes/Codes.java
+++ Felix/Codes/Codes.java
@@ -2,46 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Codes {
-  public static void main(String [] args) {
+  static Kattio io = new Kattio(System.in, System.out);
 
-    Scanner sc = new Scanner(System.in);
-      int numOfCases = sc.nextInt();
-      for (int a = 0; a < numOfCases ; a++) {
-      int row = sc.nextInt();
-      int column = sc.nextInt();
+  public static int computeDistance(int n, int k, int[][] genMatrix){
+    int minDist = n;    //The max possible value
+    int currDist = 0;   //Current distance
 
-      //Build matrix
-      int[][] matrix = new int[row][column];
-      for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
-          matrix[i][j] = sc.nextInt();
+    for (int i = 0; i < 1<<k; i++) {  //1<<k = k^2
+      int num = i;  //The number to convert to binary codeword
+      int[] codeword = new int[n];
+
+      //matrix multiplication
+      for (int x = 0; x < n; x++) {
+        int sum = 0;
+        for (int y = 0; y < k ; y++) {
+          //Travers row by row
+          //Each (bit of the row) * (a bit of the num) is summerized
+          //bit of the num is gathered though math, using shifting.
+          //we go backwards, så k-(y+1) gets ut k-(y+1)-th bit of the num
+          //The sum % 2 is the value of the codeword row by row
+          //Math magic likes shifting
+          sum += genMatrix[x][y]*((num >> k-(y+1)) & 1);
         }
+
+        //Add sum % 2 to codeword[x]
+        codeword[x] = (sum%2 == 0) ? 0 : 1;
       }
 
-      //Print matrix
-      /*
-      for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
-          System.out.print(matrix[i][j]);
-        }
-        System.out.println();
-      }
-      */
+      //Calculate current distance for codeword
+      currDist = 0;
+  		for (int c = 0; c < n; c++) {
+  			currDist += (codeword[c] == 1) ? 1 : 0;
+  		}
 
-      //Calculate minimum distance
-      int dist = 0;
-      for (int i = 0; i < column; i++) {
-        int colDist = 0;
-        for (int j = 0; j < row; j++) {
-          colDist += matrix[j][i];
-        }
-        dist += colDist % 2;
+      //if current distance is not 0, get new minimum distance
+      if(currDist != 0){
+        minDist = Math.min(minDist, currDist);
       }
-
-      System.out.println(dist);
     }
 
+    return minDist;
+  }
 
+  public static void main(String[] args) {
+    int numOfCases = io.getInt();
 
+    for (int t = 0; t < numOfCases; t++) {
+  		int n = io.getInt();
+  		int k = io.getInt();
+  		int[][] genMatrix = new  int[n][15];
+  		for (int rows = 0; rows < n; rows++) {
+  			for(int cols = 0; cols < k; cols++) {
+  				genMatrix[rows][cols] = io.getInt();
+  			}
+  		}
+      io.println(computeDistance(n, k, genMatrix));
+  	}
+
+    io.flush();
+    io.close();
   }
 }
